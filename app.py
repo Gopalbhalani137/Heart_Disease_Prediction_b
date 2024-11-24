5db118fb07ae7294dda4a886d10420aeceb342a6
Gopalbhalani <gopalbhalani137@gmail.com>
2024-11-24 14:14:16 +0530

    Updated backend configuration for port 3000

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
-from joblib import load  # Use joblib instead of pickle
+from joblib import load
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # This enables CORS for all origins
 
 # Path to the saved model
 MODEL_PATH = 'model/trained_model.joblib'
@@ -19,20 +19,15 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get input data from the request
         data = request.get_json()
         print("data in backend", data)
         if not data:
             return jsonify({"error": "No input data provided"}), 400
         
-        # Convert input data to DataFrame
         input_df = pd.DataFrame([data])
-
-        # Make prediction
         prediction = model.predict(input_df)
         prediction_proba = model.predict_proba(input_df)
 
-        # Return the result
         result = {
             "prediction": int(prediction[0]),
             "probability": {
@@ -46,4 +41,4 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(port=3000)  # Set the port to 3000
